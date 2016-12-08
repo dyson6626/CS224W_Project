a2a41b203cd0d0f1e5d051492a068fd90c61cd8b
Shen Dai <sdstc6626@gmail.com>
2016-12-07 20:22:29 -0800

    Initial Commit

---
+++ CS224w_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS224w_project
+//
+//  Created by Shen Dai on 12/7/16.
+//  Copyright © 2016 Shen Dai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
